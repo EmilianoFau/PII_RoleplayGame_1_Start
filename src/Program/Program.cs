0506d350295bf8d6d4ab78e4bad6c3621633e82d
EmilianoFau <emiliano.fau@correo.ucu.edu.uy>
2022-09-14 08:08:30 -0300

    Ejercicio adelantado

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 
-namespace Program
+namespace Roleplay_PII_Grupal
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            Item palito = new Item("Palito", "Hola", 100, 50, 0);
+            Magos mago1 = new Magos("Oswald", 100, palito);
+            Console.WriteLine(mago1.Inventario);
         }
     }
 }
